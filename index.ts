5e2d93123502d0e021b2cb1e45967de0dd511345
Mitchell Solomon <msolomonqwerty@gmail.com>
2023-11-08 09:48:58 -0500

    chore: fix path

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import Modal from './src/Modal.vue';
 import ModalLegacy from './src/ModalLegacy.vue';
 import ModalPlugin from './src/ModalPlugin.js';
-import Confirm from 'src/Confirm.vue';
-import Alert from 'src/Alert.vue';
+import Confirm from './src/Confirm.vue';
+import Alert from './src/Alert.vue';
 
 export * from './src/ModalPlugin.js';
 export * from './src/helpers.js';
