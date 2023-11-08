5b7ffdbb54db02b7ada8aa851d099fc30d4534b9
Mitchell Solomon <msolomonqwerty@gmail.com>
2023-11-08 09:47:27 -0500

    chore: export confirm and alert

--- index.ts
+++ index.ts
@@ -1,12 +1,16 @@
 import Modal from './src/Modal.vue';
 import ModalLegacy from './src/ModalLegacy.vue';
 import ModalPlugin from './src/ModalPlugin.js';
+import Confirm from 'src/Confirm.vue';
+import Alert from 'src/Alert.vue';
 
 export * from './src/ModalPlugin.js';
 export * from './src/helpers.js';
 
 export {
-Modal,
-ModalLegacy,
-ModalPlugin
+    Modal,
+    ModalLegacy,
+    ModalPlugin,
+    Confirm,
+    Alert,
 };
