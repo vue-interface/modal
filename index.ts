465a2d7d608f7f9d85fae397ff335dae4a18c151
Justin Kimbrell <jkimbrell@actengag.com>
2024-07-24 13:00:21 -0600

    fix: added modal types to package exports

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
+import Alert from './src/Alert.vue';
+import Confirm from './src/Confirm.vue';
 import Modal from './src/Modal.vue';
 import ModalLegacy from './src/ModalLegacy.vue';
 import ModalPlugin from './src/ModalPlugin.js';
-import Confirm from './src/Confirm.vue';
-import Alert from './src/Alert.vue';
 
-export * from './src/ModalPlugin.js';
 export * from './src/helpers.js';
+export type * from './src/Modal.vue';
+export * from './src/ModalPlugin.js';
 
 export {
-    Modal,
+    Alert, Confirm, Modal,
     ModalLegacy,
-    ModalPlugin,
-    Confirm,
-    Alert,
+    ModalPlugin
 };
+
